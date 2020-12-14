d141628bd9633abb77a33cabcf8ece85e1fc9504
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-12-14 16:11:10 -0700

    fix express in app.use

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use("/api");
+  app.use(express.static("/api"));
 }
 
 app.get("*", (req, res) => {
