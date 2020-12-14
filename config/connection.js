e992cbaecfa7f6232278df31fed9d3cc593e69d8
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-12-14 16:06:24 -0700

    trying to connect client

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
 // the name of the project/website should go in the placeholder.
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://TRemigi:Tj3254082!@rolodeck-native.9yh77.mongodb.net/rolodeck-native?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
--- seeders/seeds.js
+++ seeders/seeds.js
@@ -1,22 +1,19 @@
-const faker = require('faker');
+const faker = require("faker");
 
-const db = require('../config/connection');
-const { User } = require('../models');
+const db = require("../config/connection");
+const { User } = require("../models");
 
-db.once('open', async () => {
-  await User.remove({});
-
-  // create user data
-  const userData = [];
+db.once("open", async () => {
+  await User.deleteMany({});
 
   for (let i = 0; i < 50; i += 1) {
     const username = faker.internet.userName();
     const email = faker.internet.email(username);
     const password = faker.internet.password();
 
-    userData.push({ username, email, password });
+    await User.create({ username, email, password });
   }
 
-  console.log('all done!');
+  console.log("all done!");
   process.exit(0);
 });
