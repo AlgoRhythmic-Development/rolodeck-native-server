90916b0a516b278072ded1768ff7feb1c3908946
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-12-31 12:55:21 -0700

    adjust seeds.js to seed users, cards, then push cards to users

--- seeders/seeds.js
+++ seeders/seeds.js
@@ -4,19 +4,44 @@ const db = require("../config/connection");
 const { User, Card } = require("../models");
 
 db.once("open", async () => {
+  let cardIdArr = [];
+
   for (let i = 0; i < 10; i += 1) {
     // user data
-    const username = faker.internet.userName();
+    const username = faker.name.findName();
     const email = faker.internet.email(username);
     const password = faker.internet.password();
 
     // card data
-    const name = faker.name.findName();
+    const name = username;
     const jobTitle = faker.name.jobTitle();
     const phone = "000-000-0000";
 
-    await User.create({ username, email, password });
-    await Card.create({ name, jobTitle, phone, email, username });
+    const newUser = await User.create({ username, email, password });
+
+    const newCard = await Card.create({
+      name,
+      jobTitle,
+      phone,
+      email,
+      username,
+    });
+
+    cardIdArr.push(newCard._id);
+
+    await User.findByIdAndUpdate(
+      { _id: newUser._id },
+      { $push: { cards: newCard._id } },
+      { new: true }
+    );
+
+    if (i > 0) {
+      await User.findByIdAndUpdate(
+        { _id: newUser._id },
+        { $push: { collectedCards: cardIdArr[i - 1] } },
+        { new: true }
+      );
+    }
   }
 
   console.log("all done!");
