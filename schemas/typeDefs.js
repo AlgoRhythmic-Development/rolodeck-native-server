b8fcaa77106f63eefcecfda9c1d0bf75bb75f997
TRemigi <63087524+TRemigi@users.noreply.github.com>
2021-01-09 15:52:58 -0700

    remove userCards typeDef and resolver as they were redundant

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -44,7 +44,6 @@ const typeDefs = gql`
     me: User
     users: [User]
     user(_id: ID!): User
-    userCards(_id: ID!): User
     cards(username: String): [Card]
     card(_id: ID!): Card
     myCollection: User
